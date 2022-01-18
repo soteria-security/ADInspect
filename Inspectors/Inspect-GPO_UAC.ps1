475e1c296f5c6e3a477720cc6072b3d38ecfb114
ThoughtContagion <clittrell@soteria.io>
2022-01-18 10:24:19 -0500

    New Features

---
+++ Inspectors/Inspect-GPO_UAC.ps1
@@ -0,0 +1,31 @@
+Function Inspect-GPO_UAC {
+    #Get Domain
+    $domain = Get-ADDomain
+
+    #Get the GPO information and generate reports
+    $GPOs =  Get-GPO -All -Domain $domain.DNSRoot
+
+    $mitigatingPolicies = @()
+
+    $Strings = @("FilterAdministratorToken", "EnableUIADesktopToggle", "ConsentPromptBehaviorAdmin", "ConsentPromptBehaviorUser", "EnableInstallerDetection", "ValidateAdminCodeSignatures", "EnableSecureUIAPaths", "EnableLUA", "PromptOnSecureDesktop", "EnableVirtualization")
+
+    #Go through each Object and check its XML against $String
+    Foreach ($GPO in $GPOs)  {
+        Write-Output "Working on $($GPO.DisplayName)"
+        
+        #Get Current GPO Report (XML)
+        $CurrentGPOReport = Get-GPOReport -All -Domain $domain.DNSRoot -Server $domain.PDCEmulator -ReportType XML
+        
+        Foreach ($string in $strings){
+            If ($CurrentGPOReport -match $String)  {
+                $mitigatingPolicies += $CurrentGPOReport
+            }
+        }
+    }
+
+    If ($mitigatingPolicies.Count -eq 0) {
+        Return "No GPO found enforcing UAC prompt for elevation"
+    }
+}
+
+Return Inspect-GPO_UAC
